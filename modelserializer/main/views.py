053aca199422cf2680b7ec363eff0a1b95d36be8
kshittiz101 <chaudharykshittiz950@gmail.com>
2024-08-20 16:26:12 +0545

    post funciton is added

--- modelserializer/main/views.py
+++ modelserializer/main/views.py
@@ -3,8 +3,12 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse,JsonResponse
+import io
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def book_api(request):
     if request.method == 'GET':
         # get book id from request object
@@ -26,4 +30,20 @@ def book_api(request):
             json_data = JSONRenderer().render(serializer.data)
             # return response
             return HttpResponse(json_data,content_type ='application/json')
-
+        
+    if request.method == 'POST':
+        # Get the incoming JSON data
+        json_data = request.body
+        # Convert the JSON data into a stream
+        stream = io.BytesIO(json_data)
+        # Parse the stream into Python data
+        python_data = JSONParser().parse(stream)
+        # Pass the Python data to the serializer
+        serializer = BookSerializer(data=python_data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'msg': 'Data has been inserted successfully'}, status=201)
+        
+        # If the data is not valid
+        return JsonResponse(serializer.errors, status=400)
