1037ad713c76569ba42f878cdf49a0ff31ab89ab
kshittiz101 <chaudharykshittiz950@gmail.com>
2024-08-24 12:35:48 +0545

    working fine

--- view_set/viewset/readonlymodelviewset/admin.py
+++ view_set/viewset/readonlymodelviewset/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Book
 
 
-# @admin.register(Book)
-# class BookAdmin(admin.ModelAdmin):
-#     list_display = ['id','title','author','published_date','isbn']
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id','title','author','published_date','isbn']
