a0ef12c285e513b48c393d579649ec24ee0c759a
kshittiz101 <chaudharykshittiz950@gmail.com>
2024-08-25 14:09:41 +0545

    session authorization

--- basicAuthenticationAndPermission/basic_auth/basic_auth/urls.py
+++ basicAuthenticationAndPermission/basic_auth/basic_auth/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from main.routers import router
+from sessionauth.routers import router
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include(router.urls)),
+    path('sessionapi/',include(router.urls)),
+    path('auth/',include('rest_framework.urls',namespace='rest_framework')),
 ]
--- basicAuthenticationAndPermission/basic_auth/sessionauth/views.py
+++ basicAuthenticationAndPermission/basic_auth/sessionauth/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import AllowAny,IsAuthenticated,IsAdminUser
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookModelViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [SessionAuthentication]
+    # permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
