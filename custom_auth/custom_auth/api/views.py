118f22556df61c11a948a9917762a98b34385eb3
kshittiz101 <chaudharykshittiz950@gmail.com>
2024-09-02 08:09:39 +0545

    custom api complete

--- custom_auth/custom_auth/api/views.py
+++ custom_auth/custom_auth/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from .customauth import CustomAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class BookModelViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [CustomAuthentication]
+    permission_classes = [IsAuthenticated]
